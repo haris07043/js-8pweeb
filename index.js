c898b18287164f11cbc78d63b3b69cfc16c4ccd8
haris07043 <haris.asad07043@gmail.com>
2022-05-29 12:05:09 +0500

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,95 @@
-const a = Math.random();
-console.log(a)
+var activity=[
+  {
+  "ID": 64,
+  "UserID": 7,
+  "EmployeeID": "CLGAXO",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "Transport",
+  "CategoryName": "Activity - Productive",
+  "TypeName": "Transport - Freight Sorting",
+  "Date": "2018-02-14",
+  "Duration": "00:30",
+  "IsProcessed": false
+  },
+  {
+  "ID": 66,
+  "UserID": 7,
+  "EmployeeID": "CLGAXO",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "Picking",
+  "CategoryName": "Activity - Productive",
+  "TypeName": "Picking - Bulk",
+  "Date": "2018-02-15",
+  "Duration": "00:30",
+  "IsProcessed": false
+  },
+  {
+  "ID": 67,
+  "UserID": 7,
+  "EmployeeID": "CLGAXO",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "CPE RASS",
+  "CategoryName": "Activity - Productive",
+  "TypeName": "CPE RASS",
+  "Date": "2018-02-15",
+  "Duration": "00:15",
+  "IsProcessed": false
+  },
+  {
+  "ID": 71,
+  "UserID": 7,
+  "EmployeeID": "CLGAXO",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "Inventory",
+  "CategoryName": "Activity - Unproductive",
+  "TypeName": "Inventory Relocation's",
+  "Date": "2018-02-15",
+  "Duration": "01:30",
+  "IsProcessed": false
+  },
+  {
+  "ID": 72,
+  "UserID": 5,
+  "EmployeeID": "HENDERSA",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "CPE",
+  "CategoryName": "Activity - Productive",
+  "TypeName": "CPE",
+  "Date": "2018-02-15",
+  "Duration": "00:30",
+  "IsProcessed": false
+  },
+  {
+  "ID": 90,
+  "UserID": 5,
+  "EmployeeID": "HENDERSA",
+  "SiteName": "MULGRAVE",
+  "BusinessUnitName": "Telstra Logistics - Melbourne",
+  "AccountName": "IBM AUSTRALIA LTD",
+  "GroupName": "CPE RASS",
+  "CategoryName": "Activity - Productive",
+  "TypeName": "CPE RASS",
+  "Date": "2018-03-14",
+  "Duration": null,
+  "IsProcessed": false
+  }
+  ];
+
+  console.log(activity);
+
+  const result = activity.filter( activ=>{return activ.UserID===7});
+  let group = activity.reduce((r, a) => {
+    r[a.EmployeeID] = [...r[a.EmployeeID] || [], a];
+    return r;
+   }, {});
+   console.log("group", group);
